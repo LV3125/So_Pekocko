fd19d6df2b586e0840ba03dbf574d1223db1cb9a
LV3125 <lauravalez.pro@gmail.com>
2021-01-12 16:47:51 +0100

    Installation du package mongoose et du module mongoose-unique-validator / Création du fichier Users.js dans le dossier models / Création d'un modèle Mongoose pour un utilisateur

---
+++ backend/models/Users.js
@@ -0,0 +1,28 @@
+//USERS.JS - MODELS 
+//Modèle Mongoose pour la création d'utilisateur
+
+//Importation du package mongoose pour communiquer avec la base de données
+const mongoose = require('mongoose');
+
+//Importation du module mongoose-unique-validator pour vérifier l'utilisation d'une adresse email unique
+const uniqueValidator = require('mongoose-unique-validator');
+
+//Schéma mongoose pour un utilisateur
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/, "Veuillez entrer une adresse email correcte"]
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+//Vérification que l'adresse email est unique
+userSchema.plugin(uniqueValidator);
+
+//Exportation du modèle User dans le reste du projet
+module.exports = mongoose.model('User', userSchema);
